10138402441f9bc7aed0c033ae7f04f0efd2425d
Adam Akers <adamrakers@gmail.com>
2022-06-01 22:12:45 -0600

    frefactored the playerClick handler.

--- script.js
+++ script.js
@@ -3,20 +3,23 @@ const gameBoard = (() => {
   // Elements
   const boardEl = document.querySelector('.gameboard');
 
+
   // Game state
-  let playerTurn = false;
-  let playersPicks = [[], []];
+  let playerTurn = false; // GAME LOGIC
+  let playersPicks = [[], []]; // GAMEBOARD
   const winningScores = [
     [1, 2, 3], [4, 5, 6], [7, 8, 9],
     [1, 4, 7], [2, 5, 8], [3, 6, 9],
     [1, 5, 9], [3, 5, 7]
-  ];
+  ];  // GAMEBOARD OR GAME LOGIC
 
-  // Game logic
+  // GAME LOGIC
   function resetGame() {
     playersPicks = [[], []];
   }
 
+
+  // GAME LOGIC
   function checkWin(picks) {
     const test = winningScores.some(game => {
       return game.every(tileNum => picks.includes(tileNum));
@@ -25,6 +28,7 @@ const gameBoard = (() => {
     console.log(test);
   }
 
+  // PLAYER || GAME LOGIC
   function playerClick(e) {
     if (!e.target.classList.contains('tile')) return;
 
@@ -49,6 +53,7 @@ const gameBoard = (() => {
     // if win
       // display winner,
       // show replay button
+      // lock the board (remove event listener?)
     checkWin(playersPicks[playerIdx]);
   }
 
