3061b0713345c0f155e7a43cd16ce71511bb8f12
Adam Akers <adamrakers@gmail.com>
2022-06-02 19:13:39 -0600

    refactoring code. Created separate objects for gGameboard, Player and Gamelogic

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 
-const gameBoard = (() => {
-  // Elements
-  const boardEl = document.querySelector('.gameboard');
 
-
-  // Game state
-  let playerTurn = false; // GAME LOGIC
-  let playersPicks = [[], []]; // GAMEBOARD
-  const winningScores = [
-    [1, 2, 3], [4, 5, 6], [7, 8, 9],
-    [1, 4, 7], [2, 5, 8], [3, 6, 9],
-    [1, 5, 9], [3, 5, 7]
-  ];  // GAMEBOARD OR GAME LOGIC
-
-  // GAME LOGIC
+const Gameboard = (() => {
+  let players = [];
+  let playersPicks = [[], []];
+  
   function resetGame() {
     playersPicks = [[], []];
   }
 
+  function addPlayers(player1, player2) {
+    players.push(player1, player2);
+  }
+  
+  return { playersPicks, resetGame };
+})();
 
-  // GAME LOGIC
+
+function GameLogic() {
+  let playerTurn = false;
+
+  const winningScores = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9],
+    [1, 4, 7], [2, 5, 8], [3, 6, 9],
+    [1, 5, 9], [3, 5, 7]
+  ]; 
+
+
   function checkWin(picks) {
     const test = winningScores.some(game => {
       return game.every(tileNum => picks.includes(tileNum));
@@ -27,46 +33,53 @@ const gameBoard = (() => {
 
     console.log(test);
   }
-
-  // PLAYER || GAME LOGIC
+  
   function playerClick(e) {
-    if (!e.target.classList.contains('tile')) return;
 
-    if (e.target.classList.contains('active')) {
-      console.log('already clicked that!');
-      return;
-    };
 
-    playerTurn = !playerTurn;
-    const playerIdx = playerTurn ? 1 : 0;
+    // if (!e.target.classList.contains('tile')) return;
 
-    e.target.classList.add('active');
+    // if (e.target.classList.contains('active')) {
+    //   console.log('already clicked that!');
+    //   return;
+    // };
+
+    // playerTurn = !playerTurn;
+    // const playerIdx = playerTurn ? 1 : 0;
+
+    // e.target.classList.add('active');
     
-    if (playerTurn) {
-      e.target.textContent = 'X';
-    } else {
-      e.target.textContent = 'O';
-    }
+    // if (playerTurn) {
+    //   e.target.textContent = 'X';
+    // } else {
+    //   e.target.textContent = 'O';
+    // }
 
-    playersPicks[playerIdx].push(+e.target.dataset.tilenum);
+    // playersPicks[playerIdx].push(+e.target.dataset.tilenum);
 
-    // if win
-      // display winner,
-      // show replay button
-      // lock the board (remove event listener?)
-    checkWin(playersPicks[playerIdx]);
+    // // if win
+    //   // display winner,
+    //   // show replay button
+    //   // lock the board (remove event listener?)
+    // checkWin(playersPicks[playerIdx]);
   }
 
-
-  // Event Listeners
-  boardEl.addEventListener('click', playerClick);
-
-  return { playersPicks, resetGame };
-})();
+  return {}
+}
 
 
+function Player(name, symbol) {
 
-// TODO
-// 1. create players
-// 2. store players in playerPicks array (change name)
-//  2a. check score with playerX.picks, playerY.picks
+  let moves = [];
+
+  const addMove = () => {
+    // check that move isnt taken in gameboard
+    // 
+  }
+
+  return { name, symbol, addMove };
+}
+
+
+
+// ON START
